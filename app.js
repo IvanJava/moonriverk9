9b906ca89dc48be074263245a8b7333c95c2737d
Ivan Kozhokar <dmb14suka@gmail.com>
2022-02-19 12:01:43 -0800

    added footer/started to add smooth scroll to navmenu

--- app.js
+++ app.js
@@ -1,10 +1,27 @@
 //variables
 const hambIcon = document.querySelector('#hamb__menu');
 const menuLinks = document.querySelector('.navbar-menu');
+const navLogo = document.querySelector('#navbar__logo');
 
+//display mobile menu
 const popMenu = () => {
-    menuLinks.classList.toggle('active');
     hambIcon.classList.toggle('is-active');
+    menuLinks.classList.toggle('active');
 }
-
 hambIcon.addEventListener('click', popMenu);
+
+//show active menu when scrolling
+const highlightMenu = () => {
+    const elem = document.querySelector('.highlight');
+    const homeMenu = document.querySelector('#home-page');
+    const benefitsMenu = document.querySelector('#benefits-page');
+    const reviewsMenu = document.querySelector('#reviews-page');
+    const contactMenu = document.querySelector('#contact-page');
+    let scrollPos = window.scrollY;
+    //adds highlight class to my menu items
+    if(window.innerWidth > 760 && scrollPos < 900){
+        homeMenu.classList.add('highlight');
+        benefitsMenu.classList.remove('highlight');
+        return;
+    }
+}
